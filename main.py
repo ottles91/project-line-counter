b774af73db44ea76652d4e18ca111e863882c815
Cameron Ottley <cameron.ottley@gmail.com>
2025-07-22 16:34:02 -0600

    Started building out the main functions

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+import os
+import sys
+
+def count_total_lines(start_directory):
+    print("Counting total lines of code...")
+
+def main():
+    # NOTE: If you're not running with a command line argument then specify your pathname here
+
+    # ADD YOUR DIRECTORY PATH HERE
+    start_directory = ""
+
+    if len(sys.argv) > 1:
+        start_directory = sys.argv[1]
+
+    count_total_lines(start_directory)
+
+if __name__ == "__main__":
+    main()
+    print("Finished!")
