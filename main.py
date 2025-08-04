0aadae66d0eb34200a4d70d28351557f88040e50
Cameron Ottley <cameron.ottley@gmail.com>
2025-08-04 16:01:01 -0600

    Implemented initial counting function

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import os
 import sys
 
-def count_total_lines(start_directory):
+def count_total_lines(directory_path):
     print("Counting total lines of code...")
+    total_lines = process_subdirectories(directory_path)
+    print(f"Total lines of code: {total_lines}")
+
+    
+def process_subdirectories(directory_path):
+    total_lines = 0
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith(".py"):  # Update later to improve filtering
+                file_path = os.path.join(root, file)
+                with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+                    lines = f.readlines()
+                    total_lines += len(lines)
+    return total_lines
+
 
 def main():
     # NOTE: If you're not running with a command line argument then specify your pathname here
-
-    # ADD YOUR DIRECTORY PATH HERE
     start_directory = ""
 
     if len(sys.argv) > 1:
         start_directory = sys.argv[1]
+    else:
+        start_directory = os.path.dirname(os.path.abspath(__file__))
 
     count_total_lines(start_directory)
 
