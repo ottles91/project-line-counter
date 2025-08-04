8b1adc7cd8ede02b9a9b660ca4eafb52f969afda
Cameron Ottley <cameron.ottley@gmail.com>
2025-08-04 16:22:06 -0600

    Script now correctly counts (Swift, for now) files at a given path

--- main.py
+++ main.py
@@ -5,30 +5,27 @@ def count_total_lines(directory_path):
     print("Counting total lines of code...")
     total_lines = process_subdirectories(directory_path)
     print(f"Total lines of code: {total_lines}")
-
     
 def process_subdirectories(directory_path):
+    print("Processing subdirectories")
     total_lines = 0
     for root, _, files in os.walk(directory_path):
         for file in files:
-            if file.endswith(".py"):  # Update later to improve filtering
+            # print(f"Found file: {file}")
+            if file.lower().endswith(".swift"):
                 file_path = os.path.join(root, file)
+                # print(f"Reading {file_path}")
                 with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
                     lines = f.readlines()
+                    # print(f"  → {len(lines)} lines")
                     total_lines += len(lines)
     return total_lines
 
-
 def main():
-    # NOTE: If you're not running with a command line argument then specify your pathname here
-    start_directory = ""
-
     if len(sys.argv) > 1:
-        start_directory = sys.argv[1]
+        count_total_lines(sys.argv[1])
     else:
-        start_directory = os.path.dirname(os.path.abspath(__file__))
-
-    count_total_lines(start_directory)
+        count_total_lines(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     main()
