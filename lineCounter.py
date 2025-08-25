5dc2947c1b72b2614154387b1cbc34939231614d
Cameron Ottley <cameron.ottley@gmail.com>
2025-08-25 15:15:17 -0600

    Implemented updated main function

--- lineCounter.py
+++ lineCounter.py
@@ -164,6 +164,12 @@ def count_total_lines(directory_path, extensions=None, include_hidden=False, exc
 
 #     return total_lines, lines_by_type, skipped_files
 
+
+# ---------------------------
+# CLI Entry Point
+# ---------------------------
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Count total lines of code in a directory.",
@@ -178,11 +184,11 @@ def main():
         "--exclude", nargs="+", default=[],
         help="Directories or files to exclude (e.g., --exclude node_modules venv README.md)"
     )
-
     parser.add_argument(
         "--exclude-ext", nargs="+", default=[],
         help="File extensions to exclude when scanning all files (e.g., --exclude-ext .log .csv)"
     )
+
     args = parser.parse_args()
 
     if not args.directory:
@@ -191,9 +197,44 @@ def main():
 
     count_total_lines(args.directory, args.ext, args.include_hidden, args.exclude, args.exclude_ext)
 
+
 if __name__ == "__main__":
     try:
         main()
     except SystemExit as e:
-        if e.code == 2: 
+        if e.code == 2:
             sys.exit(2)
+
+# def main():
+#     parser = argparse.ArgumentParser(
+#         description="Count total lines of code in a directory.",
+#         epilog="Example: python lineCounter.py /my/code --ext .py .cs .js"
+#     )
+#     parser.add_argument("directory", nargs="?", help="Path to the directory to scan")
+#     parser.add_argument("--ext", nargs="+", default=None,
+#                         help="File extensions to include (e.g., --ext .py .cs). If not provided, all files are counted.")
+#     parser.add_argument("--include-hidden", action="store_true",
+#                         help="Include hidden files and directories (default: false)")
+#     parser.add_argument(
+#         "--exclude", nargs="+", default=[],
+#         help="Directories or files to exclude (e.g., --exclude node_modules venv README.md)"
+#     )
+
+#     parser.add_argument(
+#         "--exclude-ext", nargs="+", default=[],
+#         help="File extensions to exclude when scanning all files (e.g., --exclude-ext .log .csv)"
+#     )
+#     args = parser.parse_args()
+
+#     if not args.directory:
+#         parser.print_help()
+#         sys.exit(0)
+
+#     count_total_lines(args.directory, args.ext, args.include_hidden, args.exclude, args.exclude_ext)
+
+# if __name__ == "__main__":
+#     try:
+#         main()
+#     except SystemExit as e:
+#         if e.code == 2: 
+#             sys.exit(2)
