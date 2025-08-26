6fc7a2a2fd2e5c2eaa8c1e058b928bb1be2be5e2
Cameron Ottley <cameron.ottley@gmail.com>
2025-08-26 14:45:59 -0600

    Added main logic to the command line utility file

---
+++ cli.py
@@ -0,0 +1,38 @@
+import sys
+import argparse
+from .counter import count_total_lines
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Count total lines of code in a directory.",
+        epilog="Example: python lineCounter.py /my/code --ext .py .cs .js"
+    )
+    parser.add_argument("directory", nargs="?", help="Path to the directory to scan")
+    parser.add_argument("--ext", nargs="+", default=None,
+                        help="File extensions to include (e.g., --ext .py .cs). If not provided, all files are counted.")
+    parser.add_argument("--include-hidden", action="store_true",
+                        help="Include hidden files and directories (default: false)")
+    parser.add_argument(
+        "--exclude", nargs="+", default=[],
+        help="Directories or files to exclude (e.g., --exclude node_modules venv README.md)"
+    )
+    parser.add_argument(
+        "--exclude-ext", nargs="+", default=[],
+        help="File extensions to exclude when scanning all files (e.g., --exclude-ext .log .csv)"
+    )
+
+    args = parser.parse_args()
+
+    if not args.directory:
+        parser.print_help()
+        sys.exit(0)
+
+    count_total_lines(args.directory, args.ext, args.include_hidden, args.exclude, args.exclude_ext)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit as e:
+        if e.code == 2:
+            sys.exit(2)
